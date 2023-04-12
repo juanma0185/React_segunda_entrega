3d64aa1c98fd722cb6dd41b9bede068a808cce22
ruiz diaz juan manuel <juanma0185@gmail.com>
2023-04-12 19:05:57 -0300

    Update DataContext.js

--- src/components/Context/DataContext.js
+++ src/components/Context/DataContext.js
@@ -1,5 +1,4 @@
-import { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import { createContext, useState, } from "react";
 
 export const dataContext = createContext();
 
