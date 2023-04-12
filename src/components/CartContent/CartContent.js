56a82fac858e43b4439a4b85886872f41e5c7082
ruiz diaz juan manuel <juanma0185@gmail.com>
2023-04-12 16:46:02 -0300

    reacttrabajofinal

--- src/components/CartContent/CartContent.js
+++ src/components/CartContent/CartContent.js
@@ -3,19 +3,25 @@ import { dataContext } from "../Context/DataContext";
 
 import CartElements from "./CartElement";
 import CartTotal from "./CartTotal";
+import Navbar from "../Navbar/NavBar";
 
 import "../CartContent/CartContent.css";
 
 const CartContent = () => {
   const { cart } = useContext(dataContext);
 
-  return cart.length > 0 ? (
+  return (
     <>
-      <CartElements />
-      <CartTotal />
+      <Navbar />
+      {cart.length > 0 ? (
+        <>
+          <CartElements />
+          <CartTotal />
+        </>
+      ) : (
+        <h2 className='cart-message-center'>Your cart is empty</h2>
+      )}
     </>
-  ) : (
-    <h2 className='cart-message-center'>Tu carrito esta vacio</h2>
   );
 };
 
